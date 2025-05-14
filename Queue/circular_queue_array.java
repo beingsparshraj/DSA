6284d693cfe7502f7d37768876e0cc1264c0fe57
sparsh <rajsparsh.2004@gmail.com>
2025-05-14 12:55:35 +0530

    circular

--- Queue/circular_queue_array.java
+++ Queue/circular_queue_array.java
@@ -1,2 +1,28 @@
 public class circular_queue_array {
+
+    static int arr[];
+    static int size;
+    static int rear = -1;
+    static int front = -1;
+
+    //    constructor calls
+    Queue(int n) {
+        arr = new int[n];
+        this.size = n;
+    }
+
+    public static boolean isEmpty() {
+        return rear == -1 && front == -1;
+    }
+
+    public static boolean isFull() {
+        return (rear + 1) % size == front;
+    }
+
+//    enqueue
+
+
+    public static void main(String[] args) {
+
+    }
 }
